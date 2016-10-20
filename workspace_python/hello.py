5b474cb51eaf3437db6e4ba0b4dacf2c3ca5bf8a
lantian <1366829195@qq.com>
2016-10-20 16:50:36 +0800

    add a folder

---
+++ workspace_python/hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#' a test module '
+
+#__author__ = 'lantian'
+
+print 'hello'
+
+def add(x, y):
+	print x + y
+
+if __name__ == "__main__" :
+	add(1, 2)
